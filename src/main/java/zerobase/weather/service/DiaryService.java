ecd140707938990abf2c65e1223bf2099a88ddae
(GangPil_Song) <songgangpil@gmail.com>
2024-01-04 21:58:27 +0900

    04 - 02.3

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,6 +3,10 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -12,14 +16,42 @@ public class DiaryService {
     private String apiKey;
 
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        // open weather map에서 날씨 데이터 가져오기
+        String weatherData = getWeatherString();
+
+        // 받아온 날씨 json 파싱하기
+
+        // 파싱된 데이터 + 일기 값 우리 db에 넣기
 
     }
 
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-        System.out.println(apiUrl);
 
-        return "";
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(
+                        new InputStreamReader(connection.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 }
